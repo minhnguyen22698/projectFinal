3e8e55f95eeaf394b0064e7222d08d2779aa9845
minhnguyen22698 <minhnguyen22698@gmail.com>
2021-05-10 16:55:57 +0700

    Createe project

---
+++ assets/Script/HelloWorld.js
@@ -0,0 +1,19 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        tutorial:{
+            default:null,
+            type:cc.Button,
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
