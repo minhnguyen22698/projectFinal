5cd0f348b2e50a3d665c5541793430ddbc0df515
MinhNguyen22698 <minhnguyen22698@gmail.com>
2021-05-12 12:28:34 +0700

    Update tank moving

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -1,41 +1,46 @@
-const Emitter = require('Emitter')
+const Emitter = require("Emitter");
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        mainScreen: cc.Node,
-        playScreen: cc.Node,
-    },
+  properties: {
+    mainScreen: cc.Node,
+    playScreen: cc.Node,
+  },
 
-    // use this for initialization
-    onLoad: function () {
-        Emitter.instance = new Emitter()
-     //   Emitter.instance.registerEvent('changeScreen', this.changeRoute.bind(this))
-        cc.SystemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.moveLeft.bind(this))
-       cc.SystemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.moveRight.bind(this))
-    },
-    moveLeft(){
-        cc.log('moveLeft')
-       // Emitter.instance.emit('moveLeft')
-    },
-    moveRight(){
-        cc.log('moveRight')
-       // Emitter.instance.emit('moveRight')
-    },
-    changeRoute(arg) {
-        cc.log(this.screen)
-        switch (arg) {
-            case 'mainScreen': {
-                this.mainScreen.active = true;
-                this.playScreen.active = false;
-            }
-            case 'playScreen': {
-                this.mainScreen.active = false;
-                this.playScreen.active = true
-            }
-        }
-    },
-    // called every frame
-    update: function (dt) {
-    },
+  // use this for initialization
+  onLoad: function () {
+    Emitter.instance = new Emitter();
+    //   Emitter.instance.registerEvent('changeScreen', this.changeRoute.bind(this))
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+  },
+  onKeyDown(evt) {
+    switch (evt.keyCode) {
+      case cc.macro.KEY.a: {
+        Emitter.instance.emit("onMoveLeft",evt.keyCode);
+        break;
+      }
+      case cc.macro.KEY.d: {
+        Emitter.instance.emit("onMoveRight",evt.keyCode);
+        break;
+      }
+    }
+  },
+  onKeyUp() {
+      Emitter.instance.emit('stopMove')
+  },
+  changeRoute(arg) {
+    cc.log(this.screen);
+    switch (arg) {
+      case "mainScreen": {
+        this.mainScreen.active = true;
+        this.playScreen.active = false;
+      }
+      case "playScreen": {
+        this.mainScreen.active = false;
+        this.playScreen.active = true;
+      }
+    }
+  },
+  update: function (dt) {},
 });
--- assets/Script/playScreen/tank.js
+++ assets/Script/playScreen/tank.js
@@ -7,31 +7,48 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-const Emitter = require('Emitter')
+const Emitter = require("Emitter");
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
+  properties: {
+    accLeft: false,
+    speed: 0,
+    accRight: false,
+    accel: 0,
+  },
 
-    },
+  // LIFE-CYCLE CALLBACKS:
 
-    // LIFE-CYCLE CALLBACKS:
+  onLoad() {
+    Emitter.instance.registerEvent("onMoveLeft", this.onMoveLeft.bind(this));
+    Emitter.instance.registerEvent("onMoveRight", this.onMoveRight.bind(this));
+    Emitter.instance.registerEvent("stopMove", this.onStop.bind(this));
+  },
+  onMoveLeft(keycode) {
+    cc.log("Move left " + keycode);
+    var moveLeft = cc.moveBy(0.5, -50, 0);
+    this.node.runAction(moveLeft);
+  },
+  onMoveRight(keycode) {
+    cc.log("Move rigth " + keycode);
+    var moveLeft = cc.moveBy(0.5,50, 0);
+    this.node.runAction(moveLeft);
+  },
+  onStop() {
+    this.node.stopAllActions();
+  },
+  start() {},
 
-    onLoad() {
-        Emitter.instance.registerEvent('moveLeft', this.onMoveLeft.bind(this))
-        Emitter.instance.registerEvent('moveRight', this.onMoveRight.bind(this))
-    },
-    onMoveLeft() {
-        var action=cc.moveBy(0.5,50,0)
-        this.node.runAction(action)
-    },
-    onMoveRight() {
-        var action=cc.moveBy(0.5,-50,0)
-        this.node.runAction(action)
-    },
-    start() {
-
-    },
-
-    // update (dt) {},
+  update(dt) {
+    // if (this.accLeft) {
+    //     this.xSpeed -= this.accel * dt;
+    // } else if (this.accRight) {
+    //     this.xSpeed += this.accel * dt;
+    // }
+    // if ( Math.abs(this.xSpeed) > this.maxMoveSpeed ) {
+    //     this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+    // }
+    // this.node.x += this.xSpeed * dt;
+  },
 });
